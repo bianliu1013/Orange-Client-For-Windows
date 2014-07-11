e870bac8c8bb7af656a7e880b309cac5fe68208b
SeungWon Yang <flyhighsw@gmail.com>
2014-07-12 03:01:14 +0900

    It's my project for UI
    유아이 커밋

---
+++ Orange/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Orange
+{
+    
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        private bool IsLeftPanelState = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void MenuBtn_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+        	// TODO: 여기에 구현된 이벤트 처리기를 추가하십시오.
+            if(IsLeftPanelState)
+            {
+                Storyboard HideLeftPanelStoryboard = this.Resources["left_panel_hide"] as Storyboard;
+                HideLeftPanelStoryboard.Begin();
+                IsLeftPanelState = false;
+            }
+            else
+            {
+                Storyboard ShowLeftPanelStoryboard = this.Resources["left_panel_show"] as Storyboard;
+                ShowLeftPanelStoryboard.Begin();
+                IsLeftPanelState = true;
+            }
+        }
+    }
+}
---
+++ Orange/main_menuControl.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Orange
+{
+	/// <summary>
+	/// main_menuControl.xaml에 대한 상호 작용 논리
+	/// </summary>
+	public partial class main_menuControl : UserControl
+	{
+		public main_menuControl()
+		{
+			this.InitializeComponent();
+		}
+
+		private void mn_new_btn_Click(object sender, System.Windows.RoutedEventArgs e)
+		{
+			// TODO: 여기에 구현된 이벤트 처리기를 추가하십시오.
+		}
+
+		private void mn_myplaylist_btn_Click(object sender, System.Windows.RoutedEventArgs e)
+		{
+			// TODO: 여기에 구현된 이벤트 처리기를 추가하십시오.
+		}
+
+		private void mn_pop_btn_Click(object sender, System.Windows.RoutedEventArgs e)
+		{
+			// TODO: 여기에 구현된 이벤트 처리기를 추가하십시오.
+		}
+
+		private void mn_kpop_btn_Click(object sender, System.Windows.RoutedEventArgs e)
+		{
+			// TODO: 여기에 구현된 이벤트 처리기를 추가하십시오.
+		}
+
+		private void mn_jpop_btn_Click(object sender, System.Windows.RoutedEventArgs e)
+		{
+			// TODO: 여기에 구현된 이벤트 처리기를 추가하십시오.
+		}
+	}
+}
